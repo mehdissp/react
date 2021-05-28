eb1cbc2463be7cb2778d8fb1904324210530fb89
mehdi mahdavi <mehdimahdavi73@gmail.com>
2021-05-28 18:05:59 +0430

    video18

--- src/index.js
+++ src/index.js
@@ -1,29 +1,44 @@
-import React from 'react';
+import { React, Component } from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './seasonDisplay';
 
-function getBtnTxt() {
-  return 'click me in fun';
+
+class App extends Component {
+  // methods FIRST initialized when you  instanciate classes
+  // is called when class is created
+  constructor() {
+    super();
+    // only time you use this.state={}
+    this.state={
+      lat :null,
+      long:null
+    };
+    window.navigator.geolocation.getCurrentPosition(
+      position => {
+        this.setState({
+          lat :position.coords.latitude,
+          long :position.coords.longitude
+        })
+      },
+      error => {console.log(error)}
+    );
+
+  }
+
+  render() {
+ 
+    return (
+      <>
+        <p>lat :{this.state.lat}</p>
+        <p>lan :{this.state.long}</p>
+        <p>  <SeasonDisplay /></p>
+        <br />
+
+
+      </>
+    )
+  }
 }
-
-const App = () => {
-  const BtnTxt = 'Click Me!'
-
-  return (
-    <div>
-      hellossssssssssss
-      <label for="name" className="label">
-        enter name:
-    </label>
-      <input id="name" type="text" />
-      <button type="button" style={{
-        backgroundColor: 'skyblue'
-        , color: 'white'
-      }}>{getBtnTxt()}</button>
-    </div>
-
-  );
-};
-
 
 ReactDOM.render(
   <App />, document.getElementById('root')
