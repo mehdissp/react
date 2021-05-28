43c09234fdd5de28b8bf3a316ba2fb98bc2269e6
mehdi mahdavi <mehdimahdavi73@gmail.com>
2021-05-28 16:31:31 +0430

    video12

---
+++ src/approvalCard.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const ApprovalCard =()=>{
+return (
+<div className="ui card">
+    <div className="content">
+    lorasv  110 
+    </div>
+    <div className="extra content">
+                
+    <div className="ui two buttons">
+    <div className="ui basic green button">ApprovalCard</div>
+    <div className="ui basic red button">ApprovalCard</div>
+    </div>
+    </div>
+
+</div>
+
+)
+
+
+};
+export default ApprovalCard;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import CommentDetail from "./commentDetail";
-import {Comment} from "./commentDetail";
+import { Comment } from "./commentDetail";
+import ApprovalCard from "./approvalCard";
 
 
 // function getBtnTxt() {
@@ -16,12 +17,21 @@ const App = () => {
 
   return (
     <>
-      <div className="ui comments container">
-      <Comment/>
-      <CommentDetail author="ali" date="1 day" comment="salam khare"/>
-      <CommentDetail author="zirfali" date="2 day" comment="salam khare1"/>
-      <CommentDetail author="shandli" date="3 day" comment="salam khare2"/>
-   
+      <div className="ui comments container" style={{marginTop:"30px"}}>
+        <ApprovalCard/>
+        <Comment />
+        <CommentDetail author="ali"
+          date="1 day"
+          comment="salam khare" />
+        <CommentDetail
+          author="zirfali"
+          date="2 day"
+          comment="salam khare1" />
+        <CommentDetail
+          author="shandli"
+          date="3 day"
+          comment="salam khare2" />
+
       </div>
 
 
