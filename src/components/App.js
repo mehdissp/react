f33536683d2ec81b072ae3ecad02e445d1b0fb8b
mehdi mahdavi <mehdimahdavi73@gmail.com>
2021-06-03 14:08:37 +0430

    video 29

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import SearchBar from './SearchBar'
 
-const App =()=>{
-    return(
-        <div className="ui container" style={{'margin-top':'10px'}}>
-            <SearchBar/>
+const App = () => {
+    return (
+        <div className="ui container" style={{ 'margin-top': '10px' }}>
+            <SearchBar />
         </div>
     )
 }
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,23 +1,29 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+    // onInputClick(event) {
+    //     //todo
+    //     event.persist();
+    //     console.log(event)
+    // }
+
     render() {
         return (
             <div>
                 <div className="ui segment">
-                <form>
-                    <div className="ui form">
-                        <div className="field">
+                    <form>
+                        <div className="ui form">
+                            <div className="field">
                                 <label>
                                     image search
                                 </label>
-                                <input type="text"/>
-                            
+                                <input type="text" onChange={event=> console.log(event.target.value)} />
+
+                            </div>
                         </div>
-                    </div>
                     </form>
                 </div>
-              
+
             </div>
 
         )
