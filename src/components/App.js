ccc706350bf746fa74536303281788c9d5f78fd5
mehdi mahdavi <mehdimahdavi73@gmail.com>
2021-06-04 10:23:45 +0430

    video 33

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,21 @@
+import axios from 'axios';
 import React from 'react';
 import SearchBar from './SearchBar';
 
 
 
 class App extends React.Component {
-    onSearchSubmit(term) {
+  async  onSearchSubmit(term) {
         console.log(term);
+       const {data}=await axios.get('https://api.unsplash.com/search/photos', {
+            headers: {
+                authorization:'Client-ID OvwFnJezZH8gObQt7qbnhxl487pftfxRxj_XCHAz4sI'
+            },
+            params: {
+                query: term
+            }
+        });
+        console.log(data)
     }
     render() {
         return (
